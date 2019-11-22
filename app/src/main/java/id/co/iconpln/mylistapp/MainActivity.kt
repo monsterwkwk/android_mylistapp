ea90ea400cfcc6fe673a52d4634a56ac93081dcb
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-22 11:29:31 +0700

    [rahmatkurniawan811] Main - add list adapter

--- app/src/main/java/id/co/iconpln/mylistapp/MainActivity.kt
+++ app/src/main/java/id/co/iconpln/mylistapp/MainActivity.kt
@@ -2,12 +2,15 @@ package id.co.iconpln.mylistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getDataHero())
     }
 
     fun getDataHero(): Array<String> {
