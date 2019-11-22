45b557bd772c4e6662acd3a851e4367fbcb4ff65
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-22 13:32:26 +0700

    [rahmatkurniawan811] Hero Adapter - Bind View

--- app/src/main/java/id/co/iconpln/mylistapp/ListViewHeroAdapter.kt
+++ app/src/main/java/id/co/iconpln/mylistapp/ListViewHeroAdapter.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 
 class ListViewHeroAdapter(val context: Context, val listHero: ArrayList<Hero>):BaseAdapter() {
     override fun getView(index: Int, view: View?, viewGroup: ViewGroup?): View {
@@ -20,6 +22,12 @@ class ListViewHeroAdapter(val context: Context, val listHero: ArrayList<Hero>):B
 
     override fun getCount(): Int {
         return listHero.size
+    }
+
+    private inner class ViewHolder(view: View) {
+        private val tvHeroName: TextView = view.findViewById(R.id.tvTitle)
+        private val tvHeroDesciption: TextView = view.findViewById(R.id.tvDescription)
+        private val ivHeroPhoto: ImageView = view.findViewById(R.id.ivHeroImage)
     }
 
 }
