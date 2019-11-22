ca5f3d3f25062422916d7dad28894b4394577e06
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-22 13:18:09 +0700

    [rahmatkurniawan811] Hero Adapter - add base adapter

---
+++ app/src/main/java/id/co/iconpln/mylistapp/ListViewHeroAdapter.kt
@@ -0,0 +1,24 @@
+package id.co.iconpln.mylistapp
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+class ListViewHeroAdapter:BaseAdapter() {
+    override fun getView(index: Int, view: View?, viewGroup: ViewGroup?): View {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItem(index: Int): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(index: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
