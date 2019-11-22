45ab38f8bcdb1c9f16f25eece08daa96b38aeaa0
Rahmat Kurniawan <rahmatkurniawan811@gmail.com>
2019-11-22 13:52:33 +0700

    [rahmatkurniawan811] Main - show list using baseAdapter

--- app/src/main/java/id/co/iconpln/mylistapp/MainActivity.kt
+++ app/src/main/java/id/co/iconpln/mylistapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package id.co.iconpln.mylistapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -7,17 +8,26 @@ import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val list: ArrayList<Hero> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loadArrayAdapter()
+        //loadArrayAdapter()
+        loadListBaseAdapter(this)
     }
 
     fun loadArrayAdapter() {
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getDataHero())
         lvListHero.adapter = adapter
+    }
+
+    fun loadListBaseAdapter(context: Context) {
+        list.addAll(HeroesData.listDataHero)
+
+        val baseAdapter = ListViewHeroAdapter(context, list)
+        lvListHero.adapter = baseAdapter
     }
 
     fun getDataHero(): Array<String> {
